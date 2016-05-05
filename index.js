7fa1f0093bcc022a6d4927fd2a20aca2f28288c3
fetchi <junwenliu7@gmail.com>
2016-05-05 03:02:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,24 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var devCertPath = path.resolve(__dirname, 'FetchiiCert.p12');
+
+var pushConfig = {'ios': [
+  {
+   pfx: devCertPath, // P12 file only
+   bundleId: 'com.parse.fetchii',  // change to match bundleId
+   production: false // dev certificate
+  }
+ ]
+};
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: pushConfig,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
