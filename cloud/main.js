070c2467727860ec5c2e42fcdf15e796cd63b905
fetchi <junwenliu7@gmail.com>
2016-08-14 04:59:12 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -33,3 +33,34 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
 
   response.success('success');
 });
+
+Parse.Cloud.define("acceptfrndreq", function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
+  var params = request.params;
+  var user = request.user;
+
+  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  var messageText = params.text;
+  var targetUser = params.user;
+  var fromUser = params.fromuser;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only 
+  pushQuery.equalTo('user', targetUser);
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    data: {
+      alert: "Friend request accepted: " + fromUser  + " has accepted your friend request! Let's fetchii!",
+      badge: "Increment",
+      sound: "default"
+    }
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
